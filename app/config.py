551eaba1442533ed63fbc04d36087830db3c5cba
atif929 <ironbot911@gmail.com>
2026-06-15 08:35:21 +0500

    Resolve merge conflict in config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,8 @@
 import os
-<<<<<<< HEAD
 import sys
 
 APP_NAME = "MorphX"
 APP_VERSION = "1.0.0"
-=======
-
-APP_NAME = "MorphX"
-APP_VERSION = " "
->>>>>>> d26ac61649f2aceacca66d74e9b1b5fe3600d0f0
 APP_AUTHOR = "Atif"
 
 SUPPORTED_FORMATS = ["PDF", "DOCX", "PPTX", "JPG", "PNG", "JPEG"]
@@ -22,7 +16,6 @@ CONVERSION_MAP = {
     "JPEG": ["JPG", "PNG", "PDF", "DOCX", "PPTX"],
 }
 
-<<<<<<< HEAD
 DEFAULT_OUTPUT_DIR = os.path.join(os.path.expanduser("~"), "Desktop", "MorphX_Output")
 DEFAULT_IMAGE_DPI = 200
 DEFAULT_IMAGE_QUALITY = 95
@@ -34,10 +27,4 @@ else:
     _BASE = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 POPPLER_PATH = os.path.join(_BASE, "poppler_bin")
-=======
-DEFAULT_OUTPUT_DIR = os.path.join(os.path.expanduser("~"), "Desktop", "FileConverter_Output")
-DEFAULT_IMAGE_DPI = 200
-DEFAULT_IMAGE_QUALITY = 95
 
-POPPLER_PATH = r"E:\Python\AdvancePython\poppler-26.02.0\Library\bin"
->>>>>>> d26ac61649f2aceacca66d74e9b1b5fe3600d0f0
